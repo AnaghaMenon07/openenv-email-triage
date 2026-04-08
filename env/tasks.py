dc7588c048880563c5518b9cb6f85b1928432fd1
tanya296 <tanyakumar2006@gmail.com>
2026-04-08 22:56:26 +0530

    fixed tasks with graders

--- env/tasks.py
+++ env/tasks.py
@@ -1,19 +1,22 @@
-# env/tasks.py
+from .grader import grade
 
 tasks = [
     {
         "name": "easy",
         "description": "Classify the email into correct category (spam, support, internal)",
-        "required_fields": ["category"]
+        "required_fields": ["category"],
+        "grader": grade
     },
     {
         "name": "medium",
         "description": "Classify email and assign correct priority (low, medium, high)",
-        "required_fields": ["category", "priority"]
+        "required_fields": ["category", "priority"],
+        "grader": grade
     },
     {
         "name": "hard",
         "description": "Classify email, assign priority, and generate appropriate response",
-        "required_fields": ["category", "priority", "response"]
+        "required_fields": ["category", "priority", "response"],
+        "grader": grade
     }
 ]
